badcbc900625bd5ac373ab756892b6fc2238f864
Edwin Wallin <edwin.wallin@dequilla.com>
2021-08-23 17:58:01 +0200

    Initial

---
+++ src/App.svelte
@@ -0,0 +1,46 @@
+<script>
+	import Navigation from "./components/Navigation.svelte";
+</script>
+
+<main>
+	<Navigation></Navigation>
+	<section>
+		<img id="forest" src="images/nature-test-white.png"/>
+		<h1 class="title">Edwin Wallin</h1>
+		<h2 class="sub-title">Nature and wildlife</h2>
+	</section>
+</main>
+
+
+<style>
+	main {
+		background-image: url("/images/07062019-IMG_9410.jpg");
+		background-position: center;
+		background-size: cover;
+		min-height: 100vh;
+	}
+
+	section {
+		width: 100vw;
+		display: inline-block;
+		text-align: center;
+	}
+
+	#forest {
+		width: 14vw;
+		margin-top: 10vh;
+		margin-bottom: -10vh;
+	}
+
+	section h1 {
+		margin-top: 10vh;
+		font-size: 4em;
+		text-transform: uppercase;
+	}
+
+	section h2 {
+		margin-top: 0;
+		font-size: 1.6em;
+		text-transform: uppercase;
+	}
+</style>
---
+++ src/components/Navigation.svelte
@@ -0,0 +1,74 @@
+<script>
+
+</script>
+
+<nav>
+    <img id="logotype" src="images/signatur-2021-08-white.png"/>
+    <div id="right-side">
+        <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Gallery</a></li>
+            <li><a href="#">My Gear</a></li>
+            <li><a href="#">Contact</a></li>
+        </ul>
+        <ul>
+            <li><a href="https://www.instagram.com/wallinedwin/" target="_blank"><i class="fab fa-instagram"></i></a></li>
+            <li><a href="https://www.facebook.com/wallinedwin/" target="_blank"><i class="fab fa-facebook-square"></i></a></li>
+            <li><a href="https://www.youtube.com/user/dequillathe1" target="_blank"><i class="fab fa-youtube"></i></a></li>
+        </ul>
+    </div>
+</nav>
+
+<style>
+    nav {
+        width: 100%;
+        font-size: 1.4em;
+    }
+
+    a, a:visited {
+        text-decoration: none;
+        padding: 0.6em;
+
+        -webkit-transition: border-color 500ms ease, opacity 500ms ease, text-shadow 500ms ease;
+        -moz-transition: border-color 500ms ease, opacity 500ms ease, text-shadow 500ms ease;
+        -o-transition: border-color 500ms ease, opacity 500ms ease, text-shadow 500ms ease;
+        -ms-transition: border-color 500ms ease, opacity 500ms ease, text-shadow 500ms ease;
+        transition: border-color 500ms ease, opacity 500ms ease, text-shadow 500ms ease;
+
+        border-bottom: 1px solid rgb(255, 255, 255, 0);
+        color: #FFF;
+    }
+
+    a:hover {
+        -webkit-transition: border-color 500ms ease, opacity 500ms ease, text-shadow 500ms ease;
+        -moz-transition: border-color 500ms ease, opacity 500ms ease, text-shadow 500ms ease;
+        -o-transition: border-color 500ms ease, opacity 500ms ease, text-shadow 500ms ease;
+        -ms-transition: border-color 500ms ease, opacity 500ms ease, text-shadow 500ms ease;
+        transition: border-color 500ms ease, opacity 500ms ease, text-shadow 500ms ease;
+
+        border-color: #FFF;
+        opacity: 0.4;
+    }
+
+    #right-side {
+        float: right;
+    }
+
+    ul {
+        margin: 1em 2em;
+    }
+
+    ul, li {
+        display: inline-block;
+    }
+
+    li {
+        margin: 1em 0.5em;
+        list-style-type: none;
+    }
+
+    #logotype {
+        margin-left: 3.1em;
+        max-width: 9em;
+    }
+</style>
